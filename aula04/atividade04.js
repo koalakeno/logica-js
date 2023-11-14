9c80849bd0cf74dc46c5a224e0ef95c9f16665e9
Koala <142364551+koalakeno@users.noreply.github.com>
2023-11-14 00:48:29 +0000

    laço de repetição com array

---
+++ aula04/atividade04.js
@@ -0,0 +1,12 @@
+let frutas = ["🍇","🍓","🍊","🥑"]
+frutas.push("🍅")
+/*
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+*/
+
+let i = 0
+while(i < frutas.length){
+    console.log(frutas[i])
+    i++
+}
